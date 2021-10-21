743d5d5ffc52a14ef9e6fda076ec6c902b7f0101
Bhavya Tarun <80611224+Bhavya-Tarun@users.noreply.github.com>
2021-10-21 16:43:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 500);
+    canvas.position(560, 180);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('poses', gotPoses);
+}
+
+function draw(){
+    background('#969A97');
+}
+
+function modelLoaded(){
+    console.log("PoseNet-Initialised....")
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+    }
+}
